cfb51de208a9d3879b00050ec79c900320a30112
Unheard <unheard@unheard.org>
2023-10-05 14:37:24 -0400

    cleaning house

---
+++ app.py
@@ -0,0 +1,453 @@
+import os
+import discord
+from datetime import datetime, timedelta, date
+from discord.ext import commands, tasks
+import mysql.connector
+import pandas as pd
+from sqlalchemy import create_engine
+import requests
+import json
+
+
+print('BAMPhBot Booting...')
+
+#region Bot Definitions
+
+intents = discord.Intents.all()
+intents.typing = True
+intents.messages = True
+intents.message_content = True
+bot = discord.Bot(intents=intents)
+
+#endregion Bot Definitions
+
+#region Secrets
+
+with open('secrets.txt', 'r') as f:
+    data = f.read()
+    f.close()
+
+secrets = json.loads(data)
+
+db_host=secrets['HOST']
+db_user=secrets['USER']
+db_password=secrets['PASSWORD']
+db_database=secrets['DATABASE']
+
+#endregion Secrets
+
+#region Functions
+
+# DB engine
+def create_db_connection():
+  mydb = mysql.connector.connect(
+              host=db_host,
+              user=db_user,
+              password=db_password,
+              database=db_database
+          )
+  
+  return mydb
+
+# Used quote reset (when all quotes have been used)
+def reset_quotes():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "update quotes \
+           set quote_used = 0"
+    
+    db_cursor.execute(sql)
+    db_conn.commit()
+    db_conn.close()
+
+# Used photo reset (when all photos have been used)
+def reset_photos():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "update photos \
+           set used = 0"
+    
+    db_cursor.execute(sql)
+    db_conn.commit()
+    db_conn.close()
+    
+# Get last run time for photo or quote
+def last_run_time(type):
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+    
+    sql = f"select last_run from last_runs where type = '{type}'"
+    
+    db_cursor.execute(sql)
+    last_run = db_cursor.fetchall()
+    
+    db_conn.commit()
+    db_conn.close()
+    
+    return str(last_run[0][0])
+
+# Update last run time for photo or quote
+def update_last_run(type, last_run):
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = f"update last_runs set last_run = '{last_run}' where type = '{type}'"
+
+    db_cursor.execute(sql)
+
+    db_conn.commit()
+    db_conn.close()
+
+# Add quote
+def add_quote_to_db(quote, author):
+  db_conn = create_db_connection()
+  db_cursor = db_conn.cursor()
+
+  sql = f"insert into quotes \
+        (quote, author) \
+        values \
+        ('{quote}', '{author}')"
+
+  db_cursor.execute(sql)
+  db_conn.commit()
+  db_conn.close()
+   
+# Last available quote check
+def last_quote_check():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "select count(*) \
+           from quotes \
+           where quote_used = 0"
+
+    db_cursor.execute(sql)
+
+    result = db_cursor.fetchall()
+ 
+    db_conn.commit()
+    db_conn.close()
+
+    if result[0][0] == 1:
+        return True
+    else:
+        return False
+
+# Last available quote check
+def last_photo_check():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "select count(*) \
+           from photos \
+           where used = 0"
+
+    db_cursor.execute(sql)
+
+    result = db_cursor.fetchall()
+ 
+    db_conn.commit()
+    db_conn.close()
+
+    if result[0][0] == 1:
+        return True
+    else:
+        return False
+
+def get_quote():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "select id, quote, author \
+        from quotes \
+        where quote_used = 0 \
+        ORDER BY RAND() \
+        LIMIT 1;"
+    db_cursor.execute(sql)
+
+    # If this is the last available quote, reset used quotes
+    if last_quote_check() == True:
+        reset_quotes()
+    
+    # Get random quote
+    quote = db_cursor.fetchall()
+
+    quote_id = quote[0][0]
+    quote_text = quote[0][1]
+    author = quote[0][2]
+
+    # Build return string
+    rand_quote = f"{quote_text} - {author}"
+
+    # Update random quote as used
+    sql = f"update quotes \
+        set quote_used = 1 \
+        where id = {quote_id}"
+
+    db_cursor.execute(sql)
+
+    db_conn.commit()
+    db_conn.close()
+
+    return rand_quote
+
+def get_photo():
+    db_conn = create_db_connection()
+    db_cursor = db_conn.cursor()
+
+    sql = "select id, photo_link \
+        from photos \
+        where used = 0 \
+        ORDER BY RAND() \
+        LIMIT 1;"
+    db_cursor.execute(sql)
+
+    # If this is the last available quote, reset used quotes
+    if last_photo_check() == True:
+        reset_photos()
+    
+    # Get random quote
+    photo = db_cursor.fetchall()
+
+    photo_id = photo[0][0]
+    photo_link = photo[0][1]
+
+    # Update random quote as used
+    sql = f"update photos \
+        set used = 1 \
+        where id = {photo_id}"
+
+    db_cursor.execute(sql)
+
+    db_conn.commit()
+    db_conn.close()
+
+    return photo_link
+  
+def birthday_check():
+  db_conn = create_db_connection()
+  db_cursor = db_conn.cursor()
+
+  sql = "SELECT * FROM birthdays \
+         WHERE MONTH(birthday) = MONTH(curdate()) and day(birthday) = DAY(curdate())"
+  
+  db_cursor.execute(sql)
+
+  birthday = db_cursor.fetchall()
+
+  if db_cursor.rowcount > 0:
+     return f"Happy birthday {birthday[0][1]}"
+  else:
+     return False
+
+def get_chuggys_temp():
+  apiKey  = '4594a08911ae4f91b02153b634186c76aae5c855a005484083ab0be6a57a64ef'
+  appKey  = '025d97c09b7c44bc970c44af91ff31046d88fbd426ec4381a107979a1e20e899'
+  url     = f'https://api.ambientweather.net/v1/devices?apiKey={apiKey}&applicationKey={appKey}'
+
+  response = requests.get(url)
+  data = response.json()
+
+  temp = data[0]['lastData']['tempf']
+
+  return temp
+
+
+################### END FUNCTIONS ###################
+
+
+################### CRON JOBS ###################
+
+intents = discord.Intents.all()
+intents.typing = True
+intents.messages = True
+intents.message_content = True
+bot = discord.Bot(intents=intents)
+
+# Start loops
+
+@bot.event
+async def on_ready():
+    bday_check.start()
+    rand_quote.start()
+    rand_photo.start()
+
+@bot.event
+async def on_message(message):
+  channel = message.channel
+  author = message.author.id
+  messageContent = message.content.lower()
+  if "clemson" in messageContent or "clempson" in messageContent:
+    emoji = '\N{PILE OF POO}'
+    await message.add_reaction(emoji)
+    #await channel.send("Fuck clempson.")
+  elif "jeff" in messageContent or '<@804804163904340029>' in messageContent:
+    emoji = 'mynameisjeff:1096781925114466405'
+    await message.add_reaction(emoji)
+  elif "berry" in messageContent or '<@462087982523088908>' in messageContent:
+    emoji = 'berry:1096783181228814438'
+    await message.add_reaction(emoji)
+  elif "chuggy" in messageContent or '<@284719233601110016>' in messageContent:
+    emoji = 'chuggy:1148715141651763270'
+    await message.add_reaction(emoji)
+  
+  
+
+# Check for bamph birthday
+@tasks.loop(hours=24)
+async def bday_check():
+  await bot.wait_until_ready()
+  channel = bot.get_channel(1092446896158679131)
+
+  bday = birthday_check()
+
+  if bday != False:
+    await channel.send(f"{bday}")
+  
+# Send a random quote every 4 hours
+@tasks.loop(hours=24)
+async def rand_quote():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(1092446896158679131)
+    if datetime.now() - timedelta(hours=23.9) > datetime.fromisoformat(last_run_time('quote')):
+      print(f'Quote Run {datetime.now()}')
+      await channel.send(get_quote())
+      update_last_run('quote', datetime.now().isoformat())
+
+# Send a random quote every 6 hours
+@tasks.loop(hours=28)
+async def rand_photo():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(1092446896158679131)
+    if datetime.now() - timedelta(hours=27.9) > datetime.fromisoformat(last_run_time('photo')):
+      print(f'Photo Run {datetime.now()}')
+      await channel.send(get_photo())
+      update_last_run('photo', datetime.now().isoformat())
+
+# Start cron jobs
+@bot.event
+async def on_ready():
+    bday_check.start()
+    rand_quote.start()
+    rand_photo.start() 
+
+#endregion Cron Jobs
+  
+#region Slash Commands
+
+# /preggo_check
+@bot.slash_command(name="preggo_check",
+                  description="BAMPh First Ladies Pregnancy Check?",
+                  guild_ids=[692123814989004862])
+async def preggo_check(ctx):
+  def check(msg):
+    return msg.author == ctx.author and msg.channel == ctx.channel
+
+  s_preg_status = os.environ['s_preg_status']
+  v_preg_status = os.environ['v_preg_status']
+  c_preg_status = os.environ['c_preg_status']
+  
+  await ctx.respond(f"Sarah's Pregnancy Status: **{s_preg_status}** \
+                    \nVanessa's Pregnancy Status: **{v_preg_status}** \
+                    \nCollette's Pregnancy Status: **{c_preg_status}**")
+
+# /quote
+@bot.slash_command(name="quote",
+                  description="Get a random BAMPh quote.",
+                  guild_ids=[692123814989004862])
+async def quote(ctx):
+  def check(msg):
+    return msg.author == ctx.author and msg.channel == ctx.channel    
+
+  await ctx.respond(get_quote())
+
+# /add_quote
+@bot.slash_command(name="add_quote",
+                  description="Add BAMPh quote.",
+                  guild_ids=[692123814989004862])
+async def add_quote(ctx, quote, author):
+  def check(msg):
+    return msg.author == ctx.author and msg.channel == ctx.channel 
+
+  add_quote_to_db(quote, author)
+
+  await ctx.respond(f"{quote} - {author} - Added")
+
+# /mantrip
+@bot.slash_command(name="mantrip",
+                description="Days remaining til ManTrip™.",
+                guild_ids=[692123814989004862])
+async def mantrip(ctx):
+  def check(msg):
+    return msg.author == ctx.author and msg.channel == ctx.channel
+  
+  today = datetime.now().date()
+  mantrip = date(2024, 1, 12)
+  days_til = mantrip - today
+  days_til = days_til.days
+
+  await ctx.respond(f"There's {days_til} day(s) left until ManTrip 2024!")
+
+# /chuggys_temp
+@bot.slash_command(name="chuggys_temp",
+                  description="Get the tempature in Chuggy's backyard.",
+                  guild_ids=[692123814989004862])
+async def chuggys_temp(ctx):
+  def check(msg):
+    return msg.author == ctx.author and msg.channel == ctx.channel 
+
+  temp = get_chuggys_temp()
+
+  await ctx.respond(f"The current temperature in Chuggy's backyard is {temp}F.")
+
+#endregion Slash Commands
+
+#region Test Code
+
+class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
+
+@bot.slash_command() # Create a slash command
+async def button(ctx):
+    await ctx.respond("This is a button!", view=MyView()) # Send a message with our View class that contains the button
+
+#endregion Test Code
+
+#region Bot Events
+
+@bot.event
+async def on_message(message):
+  channel = message.channel
+  author = message.author.id
+  messageContent = message.content.lower()
+  if "clemson" in messageContent or "clempson" in messageContent:
+    emoji = '\N{PILE OF POO}'
+    await message.add_reaction(emoji)
+    #await channel.send("Fuck clempson.")
+  elif "jeff" in messageContent or '<@804804163904340029>' in messageContent:
+    emoji = 'mynameisjeff:1096781925114466405'
+    await message.add_reaction(emoji)
+  elif "berry" in messageContent or '<@462087982523088908>' in messageContent:
+    emoji = 'berry:1096783181228814438'
+    await message.add_reaction(emoji)
+  elif "chuggy" in messageContent or '<@284719233601110016>' in messageContent:
+    emoji = 'chuggy:1148715141651763270'
+    await message.add_reaction(emoji) 
+
+#endregion Bot Events
+
+#region Core Execution
+
+try:
+    bot.run('MTA5MjYzNDcwNzU0MTM2MDc2Mg.GPtN2w.orxNEoWtbjslz7JSYcsotVc9-FL1XZ70QIim5g')
+except:
+    print('Token err')
+
+#endregion Core Execution
+
+
+
