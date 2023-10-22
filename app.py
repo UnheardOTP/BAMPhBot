3ca3e975478b9497a60545c22a0a664e34f987b8
Unheard <unheard@unheard.org>
2023-10-22 15:43:54 -0400

    variable ref

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def get_ai_prompt():
   db_conn.commit()
   db_conn.close()
   print(ai_prompt)
-  return str(ai_prompt)
+  return str(ai_prompt[0])
 
 # Update AI Prompt
 def update_ai_prompt(prompt):
