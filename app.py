e89712d7ce9a0ce36f4c2fc081b4dd0d74151c79
Scott Dowling <spdowling@gmail.com>
2023-12-09 10:50:19 -0500

    alex

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ async def on_message(message):
     emoji = 'chuggy:1148715141651763270'
     await message.add_reaction(emoji)
   elif "alex" in messageContent or '<@770090117712314379>' in messageContent:
-    print("Alex TRIGGGGGERED")
+    await channel.send('TOOT')
     emoji = '\N{TRUMPET}'
     await message.add_reaction(emoji)
   
