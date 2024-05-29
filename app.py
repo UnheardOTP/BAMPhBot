79207182b62c9be709a09e5db7aa0a033735ceaa
UnheardOTP <71649543+UnheardOTP@users.noreply.github.com>
2024-05-29 19:02:39 -0400

    fix

--- app.py
+++ app.py
@@ -637,7 +637,7 @@ async def on_message(message):
     await message.add_reaction(emoji)
     #await channel.send("Fuck clempson.")
   # This is for ChatGPT interactions
-  elif '<@1092634707541360762>' in messageContent and message.author.id != 1092634707541360762:
+  elif ('<@1092634707541360762>' in messageContent and message.author.id != 1092634707541360762):
     msg = message.content.lstrip("<@1092634707541360762> ")
 
     print('Chat interaction started')
