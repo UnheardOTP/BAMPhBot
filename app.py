7415306c0e29d3c7add624b0c9b0a50bb708d3a0
UnheardOTP <71649543+UnheardOTP@users.noreply.github.com>
2024-08-24 20:07:57 -0400

    .

--- app.py
+++ app.py
@@ -724,10 +724,8 @@ async def top10dp(ctx):
   
   results = dp_point_rankings()
 
-  result_set ="```"
   for result in results:
-    result_set = result_set + f"{result[0]} - {result[1]}"
-  result_set = result_set + "```"
+    result_set = result_set + f">{result[0]} - {result[1]}"
     
   await ctx.respond(f"{result_set}")
 
