196d1da469444b65ed8c60d53ea65989a10c371b
UnheardOTP <71649543+UnheardOTP@users.noreply.github.com>
2024-10-18 20:00:27 -0400

    endless loop fix

--- app.py
+++ app.py
@@ -862,11 +862,11 @@ async def on_message(message):
   author_name = message.author.mention
   messageContent = message.content.lower()
 
-  if ("clemson" in messageContent or "clempson" in messageContent) and '<@1092634707541360762>' not in messageContent:
+  if ("clemson" in messageContent or "clempson" in messageContent) and message.author.id != 1092634707541360762:
     emoji = '\N{PILE OF POO}'
     await message.add_reaction(emoji)
     #await channel.send("Fuck clempson.")
-  elif ("FSU" in messageContent or "Florida State" in messageContent or "powerhouse" in messageContent) and '<@1092634707541360762>' not in messageContent:
+  elif ("FSU" in messageContent or "Florida State" in messageContent or "powerhouse" in messageContent) and message.author.id != 1092634707541360762:
     await message.channel.send("https://tenor.com/view/a-goddamn-powerhouse-anne-workin-moms-611-youre-the-greatest-of-all-time-gif-25278141")
   # This is for ChatGPT interactions
   elif ('<@1092634707541360762>' in messageContent and message.author.id != 1092634707541360762):
